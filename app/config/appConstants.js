611d0733c16590d8ecc7b80e19d22db25a2c0d18
jf2028mp <jeffry5800@gmail.com>
2018-11-06 22:33:10 -0600

    added create new order function

--- app/config/appConstants.js
+++ app/config/appConstants.js
@@ -4,6 +4,6 @@ export class UIConstants {
   static AppbarHeight = Platform.OS === 'ios' ? 44 : 56;
   static StatusbarHeight = Platform.OS === 'ios' ? 20 : 0;
   static HeaderHeight = UIConstants.AppbarHeight + UIConstants.StatusbarHeight;
-  static URL = 'http://192.168.1.3:8000/api/';
+  static URL = 'http://192.168.1.2:8000/api/';
   //static URL = 'https://fixercr.herokuapp.com/api/';
 }
